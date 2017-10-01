c32d81c5685bc9c90629b7b2dfaa36434c93718f
Ricardo Bomfim <ricardoub@gmail.com>
2017-10-01 14:21:08 -0300

    aula31 routes

---
+++ server/api/routes/routes.ts
@@ -0,0 +1,19 @@
+import { Application, Request, Response } from 'express';
+
+class Routes {
+
+  constructor(app: Application) {
+
+  }
+
+  getRoutes(app: Application): void {
+    app.route('/').get(
+      (req: Request, res: Response) => res.send('Hello, world!')
+    );
+    app.route('/ola/:nome').get(
+      (req: Request, res: Response) => res.send(`Hello, ${req.params.nome}`)
+    );
+  }
+}
+
+export default Routes;
